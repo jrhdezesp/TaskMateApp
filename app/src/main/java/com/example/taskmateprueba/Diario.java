29c97af3ce7fb41a4c6dc9648327cfb9782ab747
Jose Hernandez <123415299+jrhdezesp@users.noreply.github.com>
2025-04-02 14:47:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/taskmateprueba/Diario.java
@@ -0,0 +1,15 @@
+package com.example.taskmateprueba;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Diario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diario);
+
+    }
+}
